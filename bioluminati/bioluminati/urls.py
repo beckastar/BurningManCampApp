e6e146308c49e5d2f7bdb5b89e0c9d614bee2309
becka <becka@REBECCAs-MacBook-Pro.local>
2015-11-19 19:42:37 -0800

    added url for registration

--- bioluminati/bioluminati/urls.py
+++ bioluminati/bioluminati/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from camp.views import index, signup, profile
+from camp.views import index, signup, profile, register
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 
@@ -26,10 +26,7 @@ urlpatterns = [
     url(r'^accounts/profile/$', profile, name='profile'),
     url(r'^login/', 'django.contrib.auth.views.login', name='foo',kwargs={'template_name': 'login.html'}),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/'}),
-    url(r'^register/', CreateView.as_view(
-            template_name='register.html',
-            form_class=UserCreationForm,
-            success_url='/'
-    )),
+    url(r'^register/$', register, name='register'),
+    url(r'^confirm/$', register, name='confirm'),  
     # url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
