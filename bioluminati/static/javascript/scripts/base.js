4a2eafab04260e44fd0d366d241b07cab70c1746
becka <becka@REBECCAs-MacBook-Pro.local>
2015-12-20 22:00:16 -0800

    add help dialog for meal shifts.

--- bioluminati/static/javascript/scripts/base.js
+++ bioluminati/static/javascript/scripts/base.js
@@ -4,4 +4,8 @@ $(document).ready(function () {
 	// 	$(".datarow").toggleClass("showTable hideTable");
 	// });
 
+	$("#meal_shift_help_icon").click(function(){
+		$(".table_instructions").toggleClass("hidden shown");
+		console.log("clicked");
+	});
 });
