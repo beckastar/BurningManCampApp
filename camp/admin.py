b50881d10299e47bd4461986a002bdefaeab6e9e
Jeremy Dunck <jdunck@gmail.com>
2016-04-14 14:25:24 -0700

    Register custom user admin

--- camp/admin.py
+++ camp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import MealShift, User, Bike, Inventory, BicycleMutationInventory, BikeMutationSchedule, Vehicle
 
 admin.site.register(MealShift)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Bike)
 admin.site.register(Inventory)
 admin.site.register(BicycleMutationInventory)
