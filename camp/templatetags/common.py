04ad929a3fbb78f4abb8a037eae49d1f427fa512
Jeremy Dunck <jdunck@gmail.com>
2016-05-07 19:33:46 -0700

    Fix typo in templatetag

--- camp/templatetags/common.py
+++ camp/templatetags/common.py
@@ -11,7 +11,7 @@ register = template.Library()
 @register.simple_tag
 def image(img):
     if not (img and img.url):
-        return make_safe('No photo available.')
+        return mark_safe('No photo available.')
     else:
         return mark_safe(
             "<img src='%s'>" % conditional_escape(img.url))
